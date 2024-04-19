cd09cf0bde43ad30ac2227790c0d2e7b8c1b1ea2
Jenkibear <jenkinwork123@gmail.com>
2024-04-19 19:28:04 +1000

    successfully post blog to database and use navigate to return to home screen once blog posted

--- client/src/pages/add-blog/index.jsx
+++ client/src/pages/add-blog/index.jsx
@@ -1,6 +1,32 @@
+import { useContext } from 'react'
 import classes from './styles.module.css'
+import { GlobalContext } from '../../context'
+import axios from 'axios';
+import {useNavigate} from 'react-router-dom'
 
 export default function AddNewBlog(){
+
+    const {formData,setFormData} =useContext(GlobalContext);
+    const navigation = useNavigate();
+
+    async function handleSaveBlogToDataBase(){
+        const response=await axios.post('http://localhost:5000/api/blogs/add',{
+            title:formData.title,
+            description:formData.description
+        });
+        
+        const result=await response.data;
+        if(result){
+            setFormData({
+                title:'',
+                description:''
+            })
+            navigation('/')
+        }
+
+    }
+
+
     return <div className={classes.wrapper}>
         <h1>Add a Blog</h1>
         <div className={classes.formWrapper}>
@@ -9,13 +35,27 @@ export default function AddNewBlog(){
             placeholder='Enter Blog Title'
             id='title'
             type='text'
+            value={formData.title}
+            onChange={(e)=>setFormData(
+                {
+                    ...formData,
+                    title:e.target.value
+                }
+            )}
             />
             <textarea
             name='description'
             placeholder='Enter Blog Description'
             id='description'
+            value={formData.description}
+            onChange={(e)=>setFormData(
+                {
+                    ...formData,
+                    description:e.target.value
+                }
+            )}
             />
-            <button>Add New Blog</button>
+            <button onClick={handleSaveBlogToDataBase}>Add New Blog</button>
         </div>
     </div>
 }
