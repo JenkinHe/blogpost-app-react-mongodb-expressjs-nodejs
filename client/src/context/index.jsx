a0d1160970b339b603ed2a5153fe99566625f4dd
Jenkibear <jenkinwork123@gmail.com>
2024-04-19 19:38:06 +1000

    basic display of blogs retrieved from database

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -11,5 +11,9 @@ export default function GlobalState({children}){
         description:''
     })
 
-    return <GlobalContext.Provider value={{formData,setFormData}}>{children}</GlobalContext.Provider>;
+    const [blogList,setBlogList]= useState([]);
+    const [pending,setPending] =useState(false);
+
+
+    return <GlobalContext.Provider value={{formData,setFormData,blogList,setBlogList,pending,setPending}}>{children}</GlobalContext.Provider>;
 }
