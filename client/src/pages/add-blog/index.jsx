717e99c1c2773d606d2e78fbabc445ade1a79109
Jenkibear <jenkinwork123@gmail.com>
2024-04-19 18:59:52 +1000

    Blog post react page designed

--- client/src/pages/add-blog/index.jsx
+++ client/src/pages/add-blog/index.jsx
@@ -1,7 +1,21 @@
-
+import classes from './styles.module.css'
 
 export default function AddNewBlog(){
-    return <div>
+    return <div className={classes.wrapper}>
         <h1>Add a Blog</h1>
+        <div className={classes.formWrapper}>
+            <input
+            name='title'
+            placeholder='Enter Blog Title'
+            id='title'
+            type='text'
+            />
+            <textarea
+            name='description'
+            placeholder='Enter Blog Description'
+            id='description'
+            />
+            <button>Add New Blog</button>
+        </div>
     </div>
 }
